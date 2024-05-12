91fdc27bfcceaa00e99582d4b18b83f36ef303fd
VZarei <Engitrefinery@gmail.com>
2024-05-12 13:22:42 +0100

    just show published Article

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import ArticleModel
@@ -11,6 +13,10 @@ class ArticleListView(ListView):
     model = ArticleModel
     template_name = "article/ArticleListView.html"
     context_object_name  = "articles"
+
+    def get_queryset(self):
+        return ArticleModel.objects.filter(Status='published')
+    
 
 class ArticleDetailView(DetailView):
     model = ArticleModel
