418e4e5e42fd11f96a31ed666887190d9e01bc71
VZarei <Engitrefinery@gmail.com>
2024-05-12 13:01:07 +0100

    make some change on panel admin

--- mysite/article/admin.py
+++ mysite/article/admin.py
@@ -1,7 +1,38 @@
 from django.contrib import admin
-from .models import ArticleModel
+from .models import ArticleModel 
 
 # Register your models here.
 
 
-admin.site.register(ArticleModel)
+# admin.site.register(ArticleModel)
+
+
+@admin.register(ArticleModel)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ["Title", "Author","Created", "Edited", "Status"]
+    list_filter  = ["Status"]
+    search_fields= ["Title", "Author__username"]
+    
+    
+    readonly_fields = ("Created", "Edited",)
+
+    fieldsets = (
+
+        (None,{
+            "fields" : ("Title", "Author", )
+        }),
+        ("Important Dates",{
+            "fields" : ("Created", "Edited",)
+        }),
+        ("Content",{
+            "fields" : ("Body",)
+        })
+    
+    )
+
+
+
+
+
+    
--- mysite/article/models.py
+++ mysite/article/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.text import slugify
+
 
 
 # Create your models here.
@@ -7,34 +7,25 @@ from django.utils.text import slugify
 
 class ArticleModel(models.Model):
 
-    Title = models.CharField(max_length=150)
-
-    def __str__(self) -> str:
-        return self.Title
-    
-
-
-    Body  = models.TextField(max_length=10000)
-    Author= models.ForeignKey("auth.user", on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ("-created",)
-
-
-    edited = models.DateTimeField(auto_now=True) 
-
     STATUS_OF_ARTICLE = (
         ("checking" , "Checking"),
         ("rejected" , "Rejected"),
         ("published" , "Published"),
     )
-    status = models.CharField(max_length=15, choices=STATUS_OF_ARTICLE, default="checking")
-    slug = models.SlugField(unique=True, null=True)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.Title)
-        super().save(*args, **kwargs)
+    Title = models.CharField(max_length=150)
+    Body  = models.TextField(max_length=10000)
+    Author= models.ForeignKey("auth.user", on_delete=models.CASCADE)
+    Created = models.DateTimeField(auto_now_add=True)
+    Edited = models.DateTimeField(auto_now=True) 
+    Status = models.CharField(max_length=15, choices=STATUS_OF_ARTICLE, default="checking")
+    
+    def __str__(self) -> str:
+        return self.Title
+    
+
+    class Meta:
+        ordering = ("-Created",)
 
     
 
