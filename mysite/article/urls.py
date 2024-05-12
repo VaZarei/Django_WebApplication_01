1d66ddb3f5d97a900db9d940a1db156e6a0bf5cf
VZarei <Engitrefinery@gmail.com>
2024-05-12 13:15:48 +0100

    Make Article Detail Config

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . views import ArticleListView
+from . views import ArticleListView, ArticleDetailView
 
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name = "articleListView_url"),
+    path('', ArticleListView.as_view(), name = "ArticleListView_url"),
+    path('<int:pk>/', ArticleDetailView.as_view(), name = "ArticleDetailView_url"  )
 ]
--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import ArticleModel
 
 # Create your views here.
@@ -9,5 +9,10 @@ from .models import ArticleModel
 
 class ArticleListView(ListView):
     model = ArticleModel
-    template_name = "article/article.html"
+    template_name = "article/ArticleListView.html"
+    context_object_name  = "articles"
+
+class ArticleDetailView(DetailView):
+    model = ArticleModel
+    template_name = "article/ArticleDetailView.html"
     context_object_name  = "articles"
