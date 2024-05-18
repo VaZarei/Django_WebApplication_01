1b254fb39111efab99fea2a5e1935fcf1fe7a8be
VZarei <Engitrefinery@gmail.com>
2024-05-18 09:33:18 +0100

    Update Article is Done

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . views import ArticleListView, ArticleDetailView
+from . views import ArticleListView, ArticleDetailView, ArticleUpdateView
 
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name = "ArticleListView_url"),
-    path('<slug:slug>/', ArticleDetailView.as_view(), name = "ArticleDetailView_url"  )
+    path('<slug:slug>/', ArticleDetailView.as_view(), name = "ArticleDetailView_url"),
+    path('<slug:slug>/updateview/', ArticleUpdateView.as_view(), name = "ArticleUpdateView_url"),
+
 ]
--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,8 +1,9 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from .models import ArticleModel
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -22,3 +23,13 @@ class ArticleDetailView(DetailView):
     model = ArticleModel
     template_name = "article/ArticleDetailView.html"
     context_object_name  = "articles"
+
+
+class ArticleUpdateView(UpdateView):
+    model = ArticleModel
+    fields = ["Title", "Body"]
+    template_name = "article/ArticleUpdateView.html"
+    context_object_name = "articles"
+    success_url = reverse_lazy("ArticleListView_url" )
+
+    
